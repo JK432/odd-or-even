cb8bd81733315b3cf7da303a835599b94efd83c2
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-02 17:08:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Write a Python program for checking the given number is even or odd 
+x=int(input())
+if(x%2==0 and x!=1):
+  print("Even")
+else:
+  print("Odd")
+
+
+
+# if(x==1):
+#   print(x,"is not even or odd")
+ 
